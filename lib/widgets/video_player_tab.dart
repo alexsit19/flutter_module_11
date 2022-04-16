2bdbf1480571c39fbb65723e7f237d633a5a9c19
alexsit19 <alexsit12@gmail.com>
2022-04-17 01:44:44 +0300

    add videoPlayer, change readme

--- lib/widgets/video_player_tab.dart
+++ lib/widgets/video_player_tab.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPlayerTab extends StatelessWidget {
+class VideoPlayerTab extends StatefulWidget {
   const VideoPlayerTab({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Video player"),
+  State<VideoPlayerTab> createState() => _VideoPlayerTabState();
+}
+
+class _VideoPlayerTabState extends State<VideoPlayerTab> {
+  VideoPlayerController? _controller;
+  Future<void>? _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
     );
+    _initializeVideoPlayerFuture = _controller?.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 5,
+                  child: AspectRatio(
+                    aspectRatio: _controller?.value.aspectRatio ?? 1,
+                    child: VideoPlayer(_controller!),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: VideoProgressIndicator(
+                    _controller!,
+                    allowScrubbing: true,
+                    colors: const VideoProgressColors(
+                        backgroundColor: Colors.red,
+                        bufferedColor: Colors.lightBlue,
+                        playedColor: Colors.blueAccent),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.black,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            _controller?.play();
+                          },
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            _controller?.pause();
+                          },
+                          icon: const Icon(
+                            Icons.pause,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
